be0500d3013ca3d33418e1b09fb69d362611e727
Leandro Costa <contato@leandrocosta.pro.br>
2022-12-27 19:59:03 -0300

    Returning Contract only if requested by the owner

--- src/route-handlers/ContractByIdHandler.js
+++ src/route-handlers/ContractByIdHandler.js
@@ -1,7 +1,10 @@
+// GET /contracts/:id - This API is broken dizzy_face! it should return the contract only if it belongs to the profile calling. better fix that!
 module.exports = async function ContractByIdHandler(req,res){
     const {Contract} = req.app.get('models');
     const {id} = req.params;
-    const contract = await Contract.findOne({where: {id}});
+    const contract = await Contract.findOne({
+        where: {id}});
     if(!contract) return res.status(404).end();
+    if(contract.ContractorId !== req.profile.id) return res.status(403).end();
     res.json(contract);
 }
