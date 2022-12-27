26430bb7a7127d255eeb9739134bf9d41c2c316e
Leandro Costa <contato@leandrocosta.pro.br>
2022-12-27 19:59:19 -0300

    Implementing Test case for Not Owner Quering the Contract

--- tests/contracts.test.js
+++ tests/contracts.test.js
@@ -2,9 +2,9 @@ const app = require('../src/app');
 const supertest = require('supertest');
 const request = supertest(app);
 
-test("get /contracts/:id", async ()=>{
+test("get contract by id", async ()=>{
     const resp = await request.get('/contracts/1')
-        .set('profile_id', '1')
+        .set('profile_id', '5')
         .expect('Content-Type', /json/)
         .expect(200);
     expect(resp.body).toBeTruthy();
@@ -13,4 +13,10 @@ test("get /contracts/:id", async ()=>{
     expect(resp.body.status).toBe('terminated');
     expect(resp.body.ClientId).toBe(1);
     expect(resp.body.ContractorId).toBe(5);
+});
+
+test("get contract by id of different user", async ()=>{
+    const resp = await request.get('/contracts/3')
+        .set('profile_id', '1')
+        .expect(403);
 });
